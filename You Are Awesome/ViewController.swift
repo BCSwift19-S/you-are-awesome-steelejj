24a0bcf5862e38dd7a97f7527cb43f94768b2f70
James Steele <jwsteele1@me.com>
2019-01-12 00:08:18 -0500

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by James Steele on 1/12/19.
+//  Copyright © 2019 James Steele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
