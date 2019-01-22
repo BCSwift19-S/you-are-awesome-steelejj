5f1dbb1ed3c1a3f4cef36d3c38a6277eb2f823c9
James Steele <jwsteele1@me.com>
2019-01-22 00:21:36 -0500

    2.10 complete

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,17 +21,36 @@ class ViewController: UIViewController {
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         
-        let message1 = "You Are Awesome"
-        let message2 = "You Are Great"
-        let message3 = "You Are Amazing"
+        let messages = ["You Are Fantastic!",
+                        "You Are Great!",
+                        "You Are Amazing",
+                        "When the Genius Bar needs help, they call you",
+                        "You brighten my day",
+                        "You are da bomb",
+                        "Hey, Fabulous!",
+                        "You are tremendous!",
+                        "You've got the design skills of Jony Ive!",
+                        "I can't wait to download your app!"]
         
-        if messageLabel.text == message1 {
-            messageLabel.text = message2
-        } else if messageLabel.text == message2 {
-            messageLabel.text = message3
+        messageLabel.text = messages[index]
+        
+        if index == messages.count - 1 {
+            index = 0
         } else {
-            messageLabel.text = message1
+            index += 1
         }
+        
+//        let message1 = "You Are Awesome"
+//        let message2 = "You Are Great"
+//        let message3 = "You Are Amazing"
+//
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message3
+//        } else {
+//            messageLabel.text = message1
+//        }
 
     }
 
